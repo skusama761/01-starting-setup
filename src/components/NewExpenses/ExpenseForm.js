dc7effab7975c3d680a8344e8bd09381bbf8c4b0
Usama Shaikh <skusama761@gmail.com>
2022-07-04 12:28:21 +0530

    cards created

---
+++ src/components/NewExpenses/ExpenseForm.js
@@ -0,0 +1,15 @@
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control"></div>
+      </div>
+
+      <div></div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
