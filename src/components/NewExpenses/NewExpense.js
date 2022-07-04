8f8e2b185ea218b66eb4867ea63b6c6f53a709f8
Usama Shaikh <skusama761@gmail.com>
2022-07-04 12:27:56 +0530

    first commit cards created

---
+++ src/components/NewExpenses/NewExpense.js
@@ -0,0 +1,15 @@
+import "./NewExpense.css";
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense__controls">
+      <div classname="new-expense__control">
+        <label>Title</label>
+        <input type="text" />
+        <form></form>
+      </div>
+    </div>
+  );
+};
+
+export default NewExpense;
